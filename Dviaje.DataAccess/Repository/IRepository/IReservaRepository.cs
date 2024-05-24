5be2515c97c04a49141db7c901cafcba24e0054f
moonlight2028 <erickcolombia11@gmail.com>
2024-05-23 20:51:15 -0500

    ingreso de la interfaz Reservea completa, interfaz resena incompleta (completar)

---
+++ Dviaje.DataAccess/Repository/IRepository/IReservaRepository.cs
@@ -0,0 +1,9 @@
+﻿using Dviaje.Models;
+
+namespace Dviaje.DataAccess.Repository.IRepository
+{
+    public interface IReservaRepository : IRepository<Reserva>
+    {
+        void Update(Reserva reserva);
+    }
+}
