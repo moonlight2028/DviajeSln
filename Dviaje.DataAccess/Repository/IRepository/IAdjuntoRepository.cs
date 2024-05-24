35806348220f7986e1e9be58d9f70e9f5ce64835
Joanmore <joansebastianft9@gmail.com>
2024-05-23 20:20:02 -0500

    Se crea la carpeta repository e irepository con las clases y interfaces

---
+++ Dviaje.DataAccess/Repository/IRepository/IAdjuntoRepository.cs
@@ -0,0 +1,9 @@
+﻿using Dviaje.Models;
+
+namespace Dviaje.DataAccess.Repository.IRepository
+{
+    public interface IAdjuntoRepository : IRepository<Adjunto>
+    {
+        void Update(Adjunto adjunto);
+    }
+}
---
+++ Dviaje.DataAccess/Repository/Repository.cs
@@ -0,0 +1,39 @@
+﻿using Dviaje.DataAccess.Data;
+using Dviaje.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Dviaje.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, string? incluideProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(Expression<Func<T, bool>>? filter = null, string? incluideProperties = null, bool tracking = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
