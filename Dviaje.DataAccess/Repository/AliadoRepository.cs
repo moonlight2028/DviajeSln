2c12f3284d0ec23b334b75955fb02952bebbecbc
moonlight2028 <erickcolombia11@gmail.com>
2024-05-23 20:38:37 -0500

    ingreso de Aliado a interfaz

---
+++ Dviaje.DataAccess/Repository/AliadoRepository.cs
@@ -0,0 +1,21 @@
+﻿using Dviaje.DataAccess.Data;
+using Dviaje.DataAccess.Repository.IRepository;
+using Dviaje.Models;
+
+namespace Dviaje.DataAccess.Repository
+{
+    public class AliadoRepository : Repository<Aliado>, IAliadoRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AliadoRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Aliado aliado)
+        {
+            _db.Aliados.Update(aliado);
+        }
+    }
+}
