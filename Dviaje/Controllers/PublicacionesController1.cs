3fd16ba69da650b31b12f0afa0806cf9c6c5364c
Rick6 <APRENDIZ@BOGCPCDMF030503>
2024-05-27 20:45:35 -0500

    publicaciones

---
+++ Dviaje/Controllers/PublicacionesController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dviaje.Controllers
+{
+    public class PublicacionesController1 : Controller
+    {
+        public IActionResult Publicaciones()
+        {
+            return View();
+        }
+    }
+}
