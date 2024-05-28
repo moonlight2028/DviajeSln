11c822acbe5c9b3946705157c831a0e667e45087
Rick6 <APRENDIZ@BOGCPCDMF030503>
2024-05-27 20:58:40 -0500

    publicaciones

---
+++ Dviaje/Areas/Dviaje/Controllers/PublicacionesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dviaje.Areas.Dviaje.Controllers
+{
+    [Area("Dviaje")]
+    public class PublicacionesController : Controller
+    {
+        public IActionResult Publicaciones()
+        {
+            return View();
+        }
+    }
+}
