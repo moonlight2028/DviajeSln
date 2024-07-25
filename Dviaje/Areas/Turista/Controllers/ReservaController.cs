ad132880e0727699f40bbef7e87b63f3bab3bb63
moonlight2028 <erickcolombia11@gmail.com>
2024-07-24 19:32:54 -0500

    vistas reservas

---
+++ Dviaje/Areas/Turista/Controllers/ReservaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dviaje.Areas.Turista.Controllers
+{
+
+    [Area("Turista")]
+
+    public class ReservaController : Controller
+    {
+
+
+
+        public IActionResult Reserva()
+        {
+            return View();
+        }
+
+		public IActionResult MisReservas()
+		{
+			return View();
+		}
+
+		public IActionResult MiReserva()
+		{
+			return View();
+		}
+	}
+}
