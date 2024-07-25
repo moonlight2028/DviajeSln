b5346d8b8d28d7799bd03c4dd532dd850e6c7f60
Joanmore <joansebastianft9@gmail.com>
2024-07-24 20:30:51 -0500

    Ceacion de favoritos

---
+++ Dviaje.Models/VM/FavoritoVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Dviaje.Models.VM
+{
+    public class FavoritoVM
+    {
+        public string Img { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Dviaje/Areas/Turista/Controllers/FavoritosController.cs
@@ -0,0 +1,16 @@
+﻿using Dviaje.Models.VM;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dviaje.Areas.Turista.Controllers
+{
+    [Area("Turista")]
+    public class FavoritosController : Controller
+    {
+        public IActionResult Favoritos()
+        {
+            List<FavoritoVM> list = new List<FavoritoVM>();
+
+            return View(list);
+        }
+    }
+}
