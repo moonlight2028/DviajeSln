edd9e4e2b48f4e0896822849bd34ee1bdbaee053
moonlight2028 <erickcolombia11@gmail.com>
2024-07-25 20:32:40 -0500

    ingreso de controlador, vista y validator de resena

---
+++ Dviaje.Validators/ResenaValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dviaje.Models;
+using FluentValidation;
+
+namespace Dviaje.Validators
+{
+    public class ResenaValidator : AbstractValidator<Resena>
+    {
+        public ResenaValidator() 
+        {
+
+            RuleFor(r => r.Calificacion).NotEmpty().WithMessage("debe selecionar una calificacion");
+            RuleFor(r => r.Opinion).NotEmpty().WithMessage("debe ingresar una reseña valida");
+
+        }
+    }
+}
---
+++ Dviaje/Areas/Turista/Controllers/ResenaController.cs
@@ -0,0 +1,30 @@
+﻿using Dviaje.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dviaje.Areas.Turista.Controllers
+{
+    public class ResenaController : Controller
+    {
+
+        [Area("Turista")]
+        public IActionResult Resena ()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult Crear(int publicacionId, Resena resena)
+        {
+            if (ModelState.IsValid)
+            {
+                resena.PublicacionId = publicacionId;
+                _context.Add(resena);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Detalles", "Publicaciones", new { id = publicacionId });
+        }
+    }
+}
