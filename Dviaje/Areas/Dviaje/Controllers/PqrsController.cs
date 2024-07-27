ef3614d0978da4c8a6f559b021613726e6dbdb5e
Joanmore <joansebastianft9@gmail.com>
2024-07-26 20:31:54 -0500

    Se creo un nuevoo modelo y una nuva vista pqrs

--- Dviaje/Areas/Dviaje/Controllers/PqrsController.cs
+++ Dviaje/Areas/Dviaje/Controllers/PqrsController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dviaje.Models.VM;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dviaje.Areas.Dviaje.Controllers
 {
     [Area("Dviaje")]
     public class PqrsController : Controller
     {
-        public IActionResult Pqrs()
+        public IActionResult PQRS()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PQRS(PqrsVM pqrsVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(pqrsVM);
+
+            }
+
             return View();
         }
     }
---
+++ Dviaje/Areas/Turista/Controllers/AtencionViajeroController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dviaje.Areas.Turista.Controllers
+{
+    [Area("Turista")]
+    public class AtencionViajeroController : Controller
+    {
+        public IActionResult Atencion()
+        {
+            return View();
+        }
+    }
+}
