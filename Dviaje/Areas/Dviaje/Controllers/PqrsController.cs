96cedd5a332a6911b73c054852984ace51839926
Joanmore <joansebastianft9@gmail.com>
2024-08-08 20:01:17 -0500

    Pqrapi

--- Dviaje/Areas/Dviaje/Controllers/PqrsController.cs
+++ Dviaje/Areas/Dviaje/Controllers/PqrsController.cs
@@ -6,8 +6,18 @@ namespace Dviaje.Areas.Dviaje.Controllers
     [Area("Dviaje")]
     public class PqrsController : Controller
     {
-        public IActionResult Pqrs()
+
+        private IEnvioEmail _email;
+
+        public PqrsController(IEnvioEmail email)
         {
+            _email = email;
+        }
+
+        public async Task<IActionResult> Pqrs()
+        {
+            await _email.EnviarEmail("sex", "joansebastianft9@gmail.com", "sexNow", "todas mias", "<h1>nigga</h1>");
+
 
             return View();
         }
