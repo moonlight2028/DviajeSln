e8efb904d53f3d57d7b41ea97e0a7bfd5e85f353
Joanmore <joansebastianft9@gmail.com>
2024-08-08 20:04:36 -0500

    iEnvio api

---
+++ Dviaje.Services/IServices/IEnvioEmail.cs
@@ -0,0 +1,7 @@
+﻿namespace Dviaje.Services.IServices
+{
+    public interface IEnvioEmail
+    {
+        Task EnviarEmail(string asunto, string correoUser, string usuario, string cuerpoCorreo, string html);
+    }
+}
