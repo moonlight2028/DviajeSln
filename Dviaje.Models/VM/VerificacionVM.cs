5c105149cdcce7b262b9aefc3baf2fce6ff07c82
moonlight <erickcolombia11@gmail.com>
2024-08-16 20:33:31 -0500

    Creacion de controlador y vista de verificaciones

---
+++ Dviaje.Models/VM/VerificacionVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dviaje.Models.VM
+{
+    public class VerificacionVM
+    {
+        [Required(ErrorMessage = "El motivo es obligatorio.")]
+        public string Motivo { get; set; }
+
+        [Required(ErrorMessage = "La dirección del lugar es obligatoria.")]
+        public string Direccion { get; set; }
+
+        // Esto puede ser una lista separada por comas o un array, dependiendo de cómo se maneje el almacenamiento
+        public string? DocumentosAdjuntos { get; set; }
+
+        public string? DetallesPropiedad { get; set; }
+    }
+}
--- Dviaje.Models/Verificado.cs
+++ Dviaje.Models/Verificado.cs
@@ -8,25 +8,35 @@ namespace Dviaje.Models
         [Key]
         public int IdVerificado { get; set; }
 
-
         [Required(ErrorMessage = "La fecha de solicitud es obligatoria.")]
         [Column(TypeName = "timestamp")]
         public DateTime FechaSolicitud { get; set; }
 
-
         [Column(TypeName = "timestamp")]
         public DateTime? FechaRespuesta { get; set; }
-
 
         [Required(ErrorMessage = "El estado verificado es obligatorio.")]
         public VerificadoEstado VerificadoEstado { get; set; }
 
-
         [Required(ErrorMessage = "El ID del aliado es obligatorio.")]
         public string? IdAliado { get; set; }
 
-
         [ForeignKey("IdAliado")]
         public Aliado? Aliado { get; set; }
+
+        // Motivo
+        [Required(ErrorMessage = "El motivo es obligatorio.")]
+        public string Motivo { get; set; }
+
+        [Required(ErrorMessage = "La dirección del lugar es obligatoria.")]
+        public string Direccion { get; set; }
+
+        // Adjuntos
+        public string? DocumentosAdjuntos { get; set; }
+
+        // Información adicional sobre la propiedad
+        public string? DetallesPropiedad { get; set; }
     }
+
+
 }
