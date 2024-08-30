bfba98437fe660b5c5881867ac4e33aaf9bbe6c7
moonlight2028 <erickcolombia11@gmail.com>
2024-08-29 19:04:46 -0500

    implementacion de datos necesarios de ResenaTarjetaVM

--- Dviaje.Models/VM/ResenaTarjetaVM.cs
+++ Dviaje.Models/VM/ResenaTarjetaVM.cs
@@ -2,5 +2,28 @@
 {
     public class ResenaTarjetaVM
     {
+
+      
+
+        public int IdResena {  get; set; }
+
+        public string? Opinion { get; set; }
+
+        public int? Calificacion { get; set; }
+
+        public int? Megusta { get; set; }    
+
+
+        //Datos de usuario
+        public string? NombreUsuaro { get; set; }
+
+        public string? AvatarUrl { get; set; }
+
+        
+
+
+
+
+
     }
 }
