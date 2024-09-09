525ab6a185459f62c5f49a3f4ae2d2149165b0e1
moonlight <erickcolombia11@gmail.com>
2024-09-09 15:59:16 -0500

    perfil repository  consultas

--- Dviaje.DataAccess/Repository/PerfilRepository.cs
+++ Dviaje.DataAccess/Repository/PerfilRepository.cs
@@ -1,21 +1,46 @@
-﻿using Dviaje.DataAccess.Repository.IRepository;
+﻿using Dapper;
+using Dviaje.DataAccess.Repository.IRepository;
+using Dviaje.Models.VM;
 using System.Data;
 
 namespace Dviaje.DataAccess.Repository
 {
     public class PerfilRepository : IPerfilRepository
     {
-        // Conexión de la base de datos
         private readonly IDbConnection _db;
 
-
-        // Constructor e inyección de la conexión a la base de datos
         public PerfilRepository(IDbConnection db)
         {
             _db = db;
         }
 
+        // Obtiene el perfil del turista
+        public async Task<PerfilPublicoVM> GetPerfilTuristaAsync(string idUsuario)
+        {
+            var sql = @"
+                SELECT u.UserName AS Nombre, u.Avatar, 
+                       COUNT(r.IdReserva) AS NumeroReservas
+                FROM Usuarios u
+                LEFT JOIN Reservas r ON u.IdUsuario = r.IdUsuario
+                WHERE u.IdUsuario = @IdUsuario
+                GROUP BY u.UserName, u.Avatar";
 
+            return await _db.QueryFirstOrDefaultAsync<PerfilPublicoVM>(sql, new { IdUsuario = idUsuario });
+        }
 
+        // Obtiene el perfil del aliado
+        public async Task<PerfilPublicoVM> GetPerfilAliadoAsync(string idUsuario)
+        {
+            var sql = @"
+                SELECT a.RazonSocial, a.SitioWeb, a.Direccion, a.Verificado, 
+                       COUNT(p.IdPublicacion) AS NumeroPulicaciones, 
+                       AVG(p.Puntuacion) AS Puntuacion, a.AliadoEstado
+                FROM Aliados a
+                LEFT JOIN Publicaciones p ON a.IdAliado = p.IdAliado
+                WHERE a.IdAliado = @IdUsuario
+                GROUP BY a.RazonSocial, a.SitioWeb, a.Direccion, a.Verificado, a.AliadoEstado";
+
+            return await _db.QueryFirstOrDefaultAsync<PerfilPublicoVM>(sql, new { IdUsuario = idUsuario });
+        }
     }
 }
