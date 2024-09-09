177017a56669ac5547d04ba18ca4f65bb481b94c
moonlight <erickcolombia11@gmail.com>
2024-09-09 15:59:57 -0500

    metodos IRepository Perfil

--- Dviaje.DataAccess/Repository/IRepository/IPerfilRepository.cs
+++ Dviaje.DataAccess/Repository/IRepository/IPerfilRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace Dviaje.DataAccess.Repository.IRepository
+﻿using Dviaje.Models.VM;
+
+namespace Dviaje.DataAccess.Repository.IRepository
 {
     public interface IPerfilRepository
     {
+        Task<PerfilPublicoVM> GetPerfilTuristaAsync(string idUsuario);
+        Task<PerfilPublicoVM> GetPerfilAliadoAsync(string idUsuario);
     }
 }
