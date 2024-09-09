226c81b12e22c618e67967f98e3542a029ca698e
moonlight <erickcolombia11@gmail.com>
2024-09-09 15:21:12 -0500

    metodos en IRepository favoritos

--- Dviaje.DataAccess/Repository/IRepository/IFavoritosRepository.cs
+++ Dviaje.DataAccess/Repository/IRepository/IFavoritosRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Dviaje.DataAccess.Repository.IRepository
+﻿using Dviaje.Models.VM;
+
+namespace Dviaje.DataAccess.Repository.IRepository
 {
     public interface IFavoritosRepository
     {
+        Task<List<FavoritoTarjetaVM>> GetFavoritosByUsuarioAsync(string idUsuario);
+        Task<bool> CrearFavoritoAsync(int idPublicacion, string idUsuario);
+        Task<bool> EliminarFavoritoAsync(int idPublicacion, string idUsuario);
     }
 }
