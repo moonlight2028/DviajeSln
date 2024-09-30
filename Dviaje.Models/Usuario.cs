ee357607ce7ebc4413a27f9628c543166726bd51
Andres Baron <afbaronmorales@gmail.com>
2024-09-30 15:47:45 -0500

    Agregados campos faltantes a Usuario.

--- Dviaje.Models/Usuario.cs
+++ Dviaje.Models/Usuario.cs
@@ -9,33 +9,29 @@ namespace Dviaje.Models
         [StringLength(255)]
         public string? Avatar { get; set; }
 
-
         [StringLength(255)]
         public string? Banner { get; set; }
-
 
         [StringLength(40)]
         public string? RazonSocial { get; set; }
 
-
         [StringLength(255)]
         public string? SitioWeb { get; set; }
-
 
         [StringLength(50)]
         public string? Direccion { get; set; }
 
-
         [Required]
         public bool Verificado { get; set; }
 
-
         public int NumeroPublicaciones { get; set; }
 
+        public int NumeroReservas { get; set; }
+
+        public int NumeroResenas { get; set; }
 
         [Required]
         public AliadoEstado AliadoEstado { get; set; }
-
 
         [Column(TypeName = "decimal(2,1)")]
         public decimal Puntuacion { get; set; }
