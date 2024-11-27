94f72aa2f8ffbd0545bf2a5750e6db49d5ae477d
Cairon29 <josewjjunco@gmail.com>
2024-11-26 20:13:58 -0500

    Changes

---
+++ Dviaje/wwwroot/js/servicios/servicios.js
@@ -0,0 +1,46 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    // datos
+    let datosServicios = await obtenerDatos("/servicios");
+
+
+
+
+    // Selecciona la tabla usando su id
+    var tabla = document.getElementById('miTabla');
+
+    // Verifica si DataTables ya está cargado antes de inicializarlo
+    if (typeof $.fn.dataTable === 'function') {
+        new DataTable(tabla);  // Inicializa DataTables en la tabla
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+// Funciones
+async function obtenerDatos(url) {
+    try {
+        // Hacemos la solicitud GET
+        const respuesta = await fetch(url);
+
+        // Verificamos si la respuesta es exitosa (status 200)
+        if (!respuesta.ok) {
+            throw new Error('Error en la respuesta de la red');
+        }
+
+        // Parseamos la respuesta como JSON
+        const datos = await respuesta.json();
+
+        return datos;
+    } catch (error) {
+        // Si ocurre un error, lo mostramos
+        console.error('Hubo un error al obtener los datos:', error);
+    }
+}
